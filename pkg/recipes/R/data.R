8b28dde81177ed31785de3c1c916ac6e7fe385c3
Max Kuhn <mxkuhn@gmail.com>
2017-01-21 15:12:58 -0500

    Added sample data sets for examples

---
+++ pkg/recipes/R/data.R
@@ -0,0 +1,37 @@
+#' Biomass Data
+#'
+#' Ghugare et al (2014) contains a data set where different biomass fuels are
+#' characterized by the amount of certain molecules (carbon, hydrogen, oxygen, 
+#' nitrogen, and sulfur) and the corresponding higher heating value (HHV). 
+#' These data are from their Table S.2 of the Supplementary Materials
+#'
+#'
+#' @name biomass
+#' @aliases biomass
+#' @docType data
+#' @return \item{biomass}{a data frame} 
+#'
+#' @source Ghugare, S. B., Tiwary, S., Elangovan, V., and Tambe, S. S. (2013). 
+#' Prediction of Higher Heating Value of Solid Biomass Fuels Using Artificial 
+#' Intelligence Formalisms. \emph{BioEnergy Research}, 1-12.
+#'
+#' @keywords datasets
+NULL
+
+#' OkCupid Data
+#'
+#' These are a sample of columns of users of OkCupid dating website. The data
+#' are from Kim and Escobedo-Land (2015). 
+#'
+#' @name okc
+#' @aliases okc
+#' @docType data
+#' @return \item{okc}{a data frame} 
+#'
+#' @source Kim, A. Y., and A. Escobedo-Land. 2015. "OkCupid Data for Introductory 
+#' Statistics and Data Science Courses." \emph{Journal of Statistics Education: An 
+#' International Journal on the Teaching and Learning of Statistics}.
+#'
+#' @keywords datasets
+NULL
+
