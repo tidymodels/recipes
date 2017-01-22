ad739c36688945843cb2fc3ed2e611e143bcb8a7
Max Kuhn <mxkuhn@gmail.com>
2017-01-21 21:26:56 -0500

    Dummy variables test cases

---
+++ pkg/recipes/tests/testthat/test_dummies.R
@@ -0,0 +1,32 @@
+library(testthat)
+library(magrittr)
+library(recipes)
+
+data(okc)
+
+okc$location <- gsub(", california", "", okc$location)
+okc$diet[is.na(okc$diet)] <- "missing"
+okc <- okc[complete.cases(okc), -5]
+
+okc_fac <- data.frame(okc)
+
+test_that('dummy variables', {
+  rec <- recipe(age ~ ., data = okc)
+  dummy <- rec %>% step_dummy(~ diet  + location)
+  dummy_trained <- learn(dummy, data = okc, verbose = FALSE)
+  dummy_pred <- process(dummy_trained, newdata = okc)
+  dummy_pred <- dummy_pred[, names(dummy_pred) != "age"]
+  dummy_pred <- dummy_pred[, order(colnames(dummy_pred))]
+  dummy_pred <- as.data.frame(dummy_pred)
+  rownames(dummy_pred) <- NULL
+  
+  exp_res <- model.matrix(age ~ ., data = okc_fac)[, -1]
+  exp_res <- exp_res[, colnames(exp_res) != "age"]
+  colnames(exp_res) <- gsub("^location", "location_", colnames(exp_res))
+  colnames(exp_res) <- gsub("^diet", "diet_", colnames(exp_res))
+  colnames(exp_res) <- make.names(colnames(exp_res))
+  exp_res <- exp_res[, order(colnames(exp_res))]
+  exp_res <- as.data.frame(exp_res)
+  rownames(exp_res) <- NULL
+  expect_equal(dummy_pred, exp_res)
+})
