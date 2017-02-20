3f7351041961af4de74b14aede0b7f412ad3e81a
Max Kuhn <mxkuhn@gmail.com>
2017-02-20 10:39:01 -0500

    Per Hadley's suggestion, `add_role` is OO

--- R/roles.R
+++ R/roles.R
@@ -1,41 +1,8 @@
-## Adds a variable to the recipe$var_info table with a specific 
-## role 
-role_addition_char <- function(rec, vars, role) {
-  if(length(role) > 1) stop("A single role is required", call. = FALSE)
-  vars <- unique(vars)
-  ## check if there are newly defined variables in the list 
-  existing_var <- vars %in% rec$var_info$variable
-  if(any(!existing_var)) {
-    ## Add new variable with role
-    new_vars <- tibble(variable = vars[!existing_var], role = rep(role, sum(!existing_var)))
-    rec$var_info <- rbind(rec$var_info, new_vars)
-  } else {
-    ##   check for current roles != ""
-    vars2 <- vars[existing_var]
-    has_role <- rec$var_info$role[rec$var_info$variable %in% vars2] != ""
-    if(any(has_role)) {
-      warning("Overwriting role(s) for",
-              paste(vars2[has_role], collapse = ", ", sep = ""),
-              call. = FALSE)
-    }
-    rec$var_info$role[rec$var_info$variable %in% vars2] <- role
-  }
-  rec
-}
-
-role_addition_form <- function(rec, vars, role) {
-  check_elements(vars, allowed = NULL)
-  if(any(all.vars(vars) == "."))
-    stop("Dots cannot be used here; use explicit variable names in the formula", 
-         call. = FALSE) 
-  role_addition_char(rec = rec, all.vars(vars), role = role)
-}
-
 #' Manually Add Roles
 #' 
 #' \code{add_role} can add a role definition to an existing variable in the recipe.  
 #' 
-#' @param rec An existing \code{\link{recipe}}. 
+#' @param recipe An existing \code{\link{recipe}}. 
 #' @param vars A character string of variable names or simple formula that defines one or more variable that will be assigned to the role.
 #' @param role A character string for a single role.
 #' @param data Am optional data template that may be needed to resolve the formula (in case there is a \code{.}).
@@ -67,15 +34,51 @@ role_addition_form <- function(rec, vars, role) {
 #' rec2 <- add_role(rec2, vars = "dataset", role = "splitting variable")
 #' rec2$var_info
 #' 
-add_role <- function(rec, vars, role = "predictor", data = NULL) {
-  if(is_formula(vars) && !is.null(data)) 
-    vars <- get_rhs_vars(vars, data)
-  
-  if(is.character(vars)) {
-    out <- role_addition_char(rec = rec, vars = vars, role = role)
-  } else if(is_formula(vars)) {
-    out <- role_addition_form(rec = rec, vars = vars, role = role)
-  } else stop("`vars` should be a character vector or formula", call. = FALSE)
-  out$term_info <- out$role_info
-  out
+add_role <- function(recipe, vars, role = "predictor", data = NULL) { 
+  UseMethod("add_role", vars) 
+} 
+
+add_role.default <- function(recipe, vars, role = "predictor", data = NULL) { 
+  stop("`vars` should be either a character string or formula.", call. = FALSE)
+} 
+
+#' @export
+add_role.character <- function(recipe, vars, role = "predictor", data = NULL) {
+  if(length(role) > 1) stop("A single role is required", call. = FALSE)
+  vars <- unique(vars)
+  ## check if there are newly defined variables in the list 
+  existing_var <- vars %in% recipe$var_info$variable
+  if(any(!existing_var)) {
+    ## Add new variable with role
+    new_vars <- tibble(variable = vars[!existing_var], role = rep(role, sum(!existing_var)))
+    recipe$var_info <- rbind(recipe$var_info, new_vars)
+  } else {
+    ##   check for current roles != ""
+    vars2 <- vars[existing_var]
+    has_role <- recipe$var_info$role[recipe$var_info$variable %in% vars2] != ""
+    if(any(has_role)) {
+      warning("Overwriting role(s) for",
+              paste(vars2[has_role], collapse = ", ", sep = ""),
+              call. = FALSE)
+    }
+    recipe$var_info$role[recipe$var_info$variable %in% vars2] <- role
+  }
+  recipe
 }
+
+#' @export
+add_role.formula <- function(recipe, vars, role, data = NULL) {
+  check_elements(vars, allowed = NULL)
+  if(is_formula(vars) && !is.null(data)) {
+    vars <- get_rhs_vars(vars, data)
+  } else {
+    if(any(all.vars(vars) == "."))
+      stop("Dots cannot be used here; use explicit variable names in the formula or use `data`.", 
+           call. = FALSE) 
+    vars <- all.vars(vars) 
+  }
+  
+  add_role.character(recipe = recipe, vars = vars, role = role)
+}
+
+
