5a16af578faf8055e605adf905edee01862fbcb8
Max Kuhn <mxkuhn@gmail.com>
2017-02-27 20:18:30 -0500

    Natural spline and orth poly basis expansions for issue #22

---
+++ R/poly.R
@@ -0,0 +1,111 @@
+#' Orthogonal Polynomial Basis Functions
+#' 
+#' \code{step_poly} creates a \emph{specification} of a recipe step that will create new columns that are basis expansions of variables using orthogonal polynomials.
+#' 
+#' @inheritParams step_center
+#' @param role For model terms created by this step, what analysis role should they be assigned?. By default, the function assumes that the new columns created from the original variables will be used as predictors in a model.  
+#' @param objects A list of \code{\link[stats]{poly}} objects created once the step has been learned. 
+#' @param options A list of options for  \code{\link[stats]{poly}} which should not include \code{x} or \code{simple}. Note that the option \code{raw = TRUE} will produce the regular polynomial values (not orthogonalized).
+#' @return \code{step_poly} returns an object of class \code{step_poly}.
+#' @keywords datagen
+#' @concept preprocessing basis_expansion
+#' @export
+#' @details \code{step_poly} can new features from a single variable that enable fitting routines to model this variable in a nonlinear manner. The extent of the possible nonlinearity is determined by the \code{degree} argument of  \code{\link[stats]{poly}}. The original variables are removed from the data and new columns are added. The naming convention for the new variables is \code{varname_poly_1} and so on. 
+#' @examples 
+#' data(biomass)
+#' 
+#' biomass_tr <- biomass[biomass$dataset == "Training",]
+#' biomass_te <- biomass[biomass$dataset == "Testing",]
+#' 
+#' rec <- recipe(HHV ~ carbon + hydrogen + oxygen + nitrogen + sulfur,
+#'               data = biomass_tr)
+#' 
+#' library(magrittr)
+#' quadratic <- rec %>%
+#'   step_poly(terms = ~ carbon + hydrogen)
+#' quadratic <- learn(quadratic, training = biomass_tr)
+#' 
+#' expanded <- process(quadratic, biomass_te)
+#' expanded
+
+step_poly <- function(recipe, terms, role = "predictor", trained = FALSE, 
+                      objects = NULL, options = list(degree = 2)) {
+  add_step(
+    recipe, 
+    step_poly_new(
+      terms = terms, 
+      trained = trained,
+      role = role, 
+      objects = objects,
+      options = options)
+  )
+}
+
+step_poly_new <- function(terms = NULL, role = NA, trained = FALSE, 
+                          objects = NULL, options = NULL) {
+  step(
+    subclass = "poly", 
+    terms = terms,
+    role = role,
+    trained = trained,
+    objects = objects,
+    options = options
+  )
+}
+
+
+poly_wrapper <- function(x, args) {
+  args$x <- x  
+  args$simple <- FALSE
+  poly_obj <- do.call("poly", args)
+  
+  ## don't need to save the original data so keep 1 row
+  out <- matrix(NA, ncol = ncol(poly_obj), nrow = 1)
+  class(out) <- c("poly", "basis", "matrix")
+  attr(out, "degree") <- attr(poly_obj, "degree")
+  attr(out, "coefs") <- attr(poly_obj, "coefs")
+  out
+}
+
+#' @importFrom stats poly 
+learn.step_poly <- function(x, training, info = NULL, ...) {
+  col_names <- parse_terms_formula(x$terms, info = info) 
+  
+  obj <- lapply(training[, col_names], poly_wrapper, x$options)
+  for(i in seq(along = col_names))
+    attr(obj[[i]], "var") <- col_names[i]
+  
+  step_poly_new(terms = x$terms, role = x$role, 
+                trained = TRUE, objects = obj,
+                options = x$options)
+}
+
+#' @importFrom tibble as_tibble is_tibble
+#' @importFrom stats predict 
+process.step_poly <- function(object, newdata, ...) {
+  ## pre-allocate a matrix for the basis functions.
+  new_cols <- vapply(object$objects, ncol, c(int = 1L))
+  poly_values <- matrix(NA, nrow = nrow(newdata), ncol = sum(new_cols))
+  colnames(poly_values) <- rep("", sum(new_cols))
+  strt <- 1
+  for(i in names(object$objects)) {
+    cols <- (strt):(strt+new_cols[i]-1)
+    orig_var <- attr(object$objects[[i]], "var")
+    poly_values[, cols] <- predict(object$objects[[i]], getElement(newdata, i))
+    new_names <- paste(orig_var, "poly", names0(new_cols[i], ""), sep = "_")
+    colnames(poly_values)[cols] <- new_names
+    strt <- max(cols)+1
+    newdata[, orig_var] <- NULL
+  }
+  poly_values <- as_tibble(poly_values)
+  newdata <- cbind(newdata, poly_values)
+  if(!is_tibble(newdata)) as_tibble(newdata)
+}
+
+
+print.step_poly <- function(x, form_width = 30, ...) {
+  cat("Orthogonal polynomials on ")
+  cat(form_printer(x, wdth = form_width))
+  if(x$trained) cat(" [trained]\n") else cat("\n")
+  invisible(x)
+}
---
+++ tests/testthat/test_poly.R
@@ -0,0 +1,50 @@
+library(testthat)
+library(magrittr)
+library(recipes)
+data(biomass)
+
+biomass_tr <- biomass[biomass$dataset == "Training",]
+biomass_te <- biomass[biomass$dataset == "Testing",]
+
+rec <- recipe(HHV ~ carbon + hydrogen + oxygen + nitrogen + sulfur,
+              data = biomass_tr)
+
+test_that('correct basis functions', {
+  with_poly <- rec %>% 
+    step_poly(~ carbon + hydrogen)
+  
+  with_poly <- learn(with_poly, training = biomass_tr, verbose = FALSE)
+  
+  with_poly_pred_tr <- process(with_poly, newdata = biomass_tr)
+  with_poly_pred_te <- process(with_poly, newdata = biomass_te)
+  
+  carbon_poly_tr_exp <- poly(biomass_tr$carbon, degree = 2)
+  hydrogen_poly_tr_exp <- poly(biomass_tr$hydrogen, degree = 2)
+  carbon_poly_te_exp <- predict(carbon_poly_tr_exp, biomass_te$carbon)
+  hydrogen_poly_te_exp <- predict(hydrogen_poly_tr_exp, biomass_te$hydrogen)
+  
+  carbon_poly_tr_res <- as.matrix(with_poly_pred_tr[, grep("carbon", names(with_poly_pred_tr))])
+  colnames(carbon_poly_tr_res) <- NULL
+  hydrogen_poly_tr_res <- as.matrix(with_poly_pred_tr[, grep("hydrogen", names(with_poly_pred_tr))])
+  colnames(hydrogen_poly_tr_res) <- NULL
+  
+  carbon_poly_te_res <- as.matrix(with_poly_pred_te[, grep("carbon", names(with_poly_pred_te))])
+  colnames(carbon_poly_te_res) <- 1:ncol(carbon_poly_te_res)
+  hydrogen_poly_te_res <- as.matrix(with_poly_pred_te[, grep("hydrogen", names(with_poly_pred_te))])
+  colnames(hydrogen_poly_te_res) <- 1:ncol(hydrogen_poly_te_res)
+  
+  ## remove attributes
+  carbon_poly_tr_exp <- matrix(carbon_poly_tr_exp, ncol = 2)
+  carbon_poly_te_exp <- matrix(carbon_poly_te_exp, ncol = 2)
+  hydrogen_poly_tr_exp <- matrix(hydrogen_poly_tr_exp, ncol = 2)
+  hydrogen_poly_te_exp <- matrix(hydrogen_poly_te_exp, ncol = 2) 
+  dimnames(carbon_poly_tr_res) <- NULL
+  dimnames(carbon_poly_te_res) <- NULL  
+  dimnames(hydrogen_poly_tr_res) <- NULL
+  dimnames(hydrogen_poly_te_res) <- NULL  
+  
+  expect_equal(carbon_poly_tr_res, carbon_poly_tr_exp)
+  expect_equal(carbon_poly_te_res, carbon_poly_te_exp)
+  expect_equal(hydrogen_poly_tr_res, hydrogen_poly_tr_exp)
+  expect_equal(hydrogen_poly_te_res, hydrogen_poly_te_exp)  
+})
