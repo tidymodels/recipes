6948141c017be2a3243be374e186249bf5949d75
Max Kuhn <mxkuhn@gmail.com>
2017-05-08 23:20:53 -0400

    Added a small data set

--- R/data.R
+++ R/data.R
@@ -61,3 +61,21 @@ NULL
 #' str(credit_data)
 NULL
 
+
+
+#' Raw Cover Type Data
+#'
+#' These data are raw data describing different types of forest cover-types from the UCI Machine Learning Database (see link below). There is one column in the data that has a few difference pieces of textual information (of variable lengths). 
+#'
+#' @name covers
+#' @aliases covers
+#' @docType data
+#' @return \item{covers}{a data frame} 
+#'
+#' @source \url{https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.info}
+#'
+#' @keywords datasets
+#' @examples 
+#' data(covers)
+#' str(covers)
+NULL
