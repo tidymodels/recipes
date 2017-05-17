d8893def366e6e09635e498ea3382a9aafb84d51
Max Kuhn <mxkuhn@gmail.com>
2017-05-17 10:31:04 -0700

    Ratio steps for issue #15

---
+++ R/ratio.R
@@ -0,0 +1,161 @@
+#' Ratio Variable Creation
+#'
+#' \code{step_ratio} creates a a \emph{specification} of a recipe step that
+#'   will create one or more ratios out of numeric variables.
+#'
+#' @inheritParams step_center
+#' @param ... One or more selector functions to choose which variables will
+#'   be used in the \emph{numerator} of the ratio. When used with
+#'   \code{denom_vars}, the dots indicates which variables are used in the
+#'   \emph{denominator}. See \code{\link{selections}} for more details.
+#' @param role For terms created by this step, what analysis role should
+#'   they be assigned?. By default, the function assumes that the newly created
+#'   ratios created by the original variables will be used as
+#'   predictors in a model.
+#' @param denom A call to \code{denom_vars} to specify which variables are
+#'   used in the denominator that can include specific variable names
+#'   separated by commas or different selectors (see
+#'   \code{\link{selections}}).  If a column is included in both lists to be
+#'   numerator and denominator, it will be removed from the listing.
+#' @param naming A function that defines the naming convention for new ratio
+#'   columns.
+#' @param variables The column names used in the rations. This argument is
+#'   not populated until \code{\link{learn.recipe}} is executed.
+#' @return \code{step_ratio} returns an object of class \code{step_ratio}.
+#' @keywords datagen
+#' @concept preprocessing
+#' @export
+#' @examples 
+#' library(recipes)
+#' data(biomass)
+#' 
+#' biomass$total <- apply(biomass[, 3:7], 1, sum)
+#' biomass_tr <- biomass[biomass$dataset == "Training",]
+#' biomass_te <- biomass[biomass$dataset == "Testing",]
+#' 
+#' rec <- recipe(HHV ~ carbon + hydrogen + oxygen + nitrogen + 
+#'                     sulfur + total,
+#'               data = biomass_tr)
+#' 
+#' ratio_recipe <- rec %>%
+#'   # all predictors over total
+#'   step_ratio(all_predictors(), denom = denom_vars(total)) %>%
+#'   # get rid of the original predictors 
+#'   step_rm(all_predictors(), -matches("_o_"))
+#'   
+#' 
+#' ratio_recipe <- learn(ratio_recipe, training = biomass_tr)
+#' 
+#' ratio_data <- process(ratio_recipe, biomass_te)
+#' ratio_data
+
+step_ratio <-
+  function(recipe,
+           ...,
+           role = "predictor",
+           trained = FALSE,
+           denom = denom_vars(),
+           naming = function(numer, denom)
+             make.names(paste(numer, denom, sep = "_o_")),
+           variables = NULL) {
+    
+    terms <- quos(...)
+    if (is_empty(terms))
+      stop("Please supply at least one numerator variable specification. ",
+           "See ?selections.", call. = FALSE)
+    if (is_empty(denom))
+      stop("Please supply at least one denominator variable specification. ",
+           "See ?selections.", call. = FALSE)
+    add_step(
+      recipe,
+      step_ratio_new(
+        terms = terms,
+        role = role,
+        trained = trained,
+        denom = denom,
+        naming = naming,
+        variables = variables
+      )
+    )
+  }
+
+step_ratio_new <-
+  function(terms = NULL,
+           role = "predictor",
+           trained = FALSE,
+           denom = NULL,
+           naming = NULL,
+           variables = NULL
+  ) {
+    step(
+      subclass = "ratio",
+      terms = terms,
+      role = role,
+      trained = trained,
+      denom = denom,
+      naming = naming,
+      variables = variables
+    )
+  }
+
+
+#' @export
+learn.step_ratio <- function(x, training, info = NULL, ...) {
+  col_names <- expand.grid(
+    top = select_terms(x$terms, info = info),
+    bottom = select_terms(x$denom, info = info),
+    stringsAsFactors = FALSE
+  )
+  col_names <- col_names[!(col_names$top == col_names$bottom), ]
+  
+  if (nrow(col_names) == 0)
+    stop("No variables were selected for making ratios", call. = FALSE)
+  if (any(info$type[info$variable %in% col_names$top] != "numeric"))
+    stop("The ratio variables should be numeric")
+  if (any(info$type[info$variable %in% col_names$bottom] != "numeric"))
+    stop("The ratio variables should be numeric")
+  
+  step_ratio_new(
+    terms = x$terms,
+    role = x$role,
+    trained = TRUE,
+    denom = x$denom,
+    naming = x$naming,
+    variables = col_names
+  )
+}
+
+#' @export
+process.step_ratio <- function(object, newdata, ...) {
+  res <- newdata[, object$variables$top] /
+    newdata[, object$variables$bottom]
+  colnames(res) <-
+    apply(object$variables, 1, function(x)
+      object$naming(x[1], x[2]))
+  if (!is_tibble(res))
+    res <- as_tibble(res)
+  
+  newdata <- cbind(newdata, res)
+  if (!is_tibble(newdata))
+    newdata <- as_tibble(newdata)
+  newdata
+}
+
+print.step_ratio <-
+  function(x, width = max(20, options()$width - 30), ...) {
+    cat("Ratios from ")
+    if (x$trained) {
+      vars <- c(unique(x$variables$top), unique(x$variables$bottom))
+      cat(format_ch_vec(vars, width = width))
+    } else
+      cat(format_selectors(c(x$terms, x$denom), wdth = width))
+    if (x$trained)
+      cat(" [trained]\n")
+    else
+      cat("\n")
+    invisible(x)
+  }
+
+#' @export
+#' @rdname step_ratio
+denom_vars <- function(...) quos(...)
---
+++ tests/testthat/test_ratio.R
@@ -0,0 +1,90 @@
+library(testthat)
+library(magrittr)
+library(recipes)
+library(tibble)
+
+n <- 20
+ex_dat <- data.frame(
+  x1 = -1:8,
+  x2 = 1,
+  x3 = c(1:9, NA),
+  x4 = 11:20,
+  x5 = letters[1:10]
+)
+
+rec <- recipe( ~ x1 + x2 + x3 + x4 + x5, data = ex_dat)
+
+test_that('1:many', {
+  rec1 <- rec %>% 
+    step_ratio(x1, denom = denom_vars(all_numeric()))
+  rec1 <- learn(rec1, ex_dat, verbose = FALSE)
+  obs1 <- process(rec1, ex_dat)
+  res1 <- tibble(
+    x1_o_x2   = ex_dat$x1/ex_dat$x2,
+    x1_o_x3   = ex_dat$x1/ex_dat$x3,
+    x1_o_x4   = ex_dat$x1/ex_dat$x4
+  )
+  for(i in names(res1)) 
+    expect_equal(res1[i], obs1[i])
+})
+
+
+test_that('many:1', {
+  rec2 <- rec %>% 
+    step_ratio(all_numeric(), denom = denom_vars(x1))
+  rec2 <- learn(rec2, ex_dat, verbose = FALSE)
+  obs2 <- process(rec2, ex_dat)
+  res2 <- tibble(
+    x2_o_x1   = ex_dat$x2/ex_dat$x1,
+    x3_o_x1   = ex_dat$x3/ex_dat$x1,
+    x4_o_x1   = ex_dat$x4/ex_dat$x1
+  )
+  for(i in names(res2)) 
+    expect_equal(res2[i], obs2[i])
+})
+
+
+test_that('many:many', {
+  rec3 <- rec %>% 
+    step_ratio(all_numeric(), denom = denom_vars(all_numeric()))
+  rec3 <- learn(rec3, ex_dat, verbose = FALSE)
+  obs3 <- process(rec3, ex_dat)
+  res3 <- tibble(
+    x2_o_x1   = ex_dat$x2/ex_dat$x1,
+    x3_o_x1   = ex_dat$x3/ex_dat$x1,
+    x4_o_x1   = ex_dat$x4/ex_dat$x1,
+
+    x1_o_x2   = ex_dat$x1/ex_dat$x2,
+    x3_o_x2   = ex_dat$x3/ex_dat$x2,
+    x4_o_x2   = ex_dat$x4/ex_dat$x2,
+
+    x1_o_x3   = ex_dat$x1/ex_dat$x3,
+    x2_o_x3   = ex_dat$x2/ex_dat$x3,
+    x4_o_x3   = ex_dat$x4/ex_dat$x3,   
+    
+    x1_o_x4   = ex_dat$x1/ex_dat$x4,
+    x2_o_x4   = ex_dat$x2/ex_dat$x4,
+    x3_o_x4   = ex_dat$x3/ex_dat$x4
+  )
+  for(i in names(res3)) 
+    expect_equal(res3[i], obs3[i])
+})
+
+
+
+test_that('wrong type', {
+  rec4 <- rec %>% 
+    step_ratio(x1, denom = denom_vars(all_predictors()))
+  expect_error(learn(rec4, ex_dat, verbose = FALSE))
+
+  rec5 <- rec %>% 
+    step_ratio(all_predictors(), denom = denom_vars(x1))
+  expect_error(learn(rec5, ex_dat, verbose = FALSE))
+  
+  rec6 <- rec %>% 
+    step_ratio(all_predictors(), denom = denom_vars(all_predictors()))
+  expect_error(learn(rec6, ex_dat, verbose = FALSE))  
+})
+
+
+
