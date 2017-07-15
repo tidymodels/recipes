4f391755199365bad83b296f6e7eac15420b27a2
Alex Hayes <alexpghayes@gmail.com>
2017-07-14 17:00:51 -0700

    update step_intercept tests after consistency tweaks

--- tests/testthat/test_intercept.R
+++ tests/testthat/test_intercept.R
@@ -41,25 +41,14 @@ test_that('deals with bad input', {
     recipe(~ ., data = ex_dat) %>%
       step_intercept(name = 4) %>%
       prepare(),
-    "Intercept/constant column name must be character."
+    "character"
   )
 
   expect_warning(
     recipe(~ ., data = ex_dat) %>%
       step_intercept(all_predictors()) %>%
       prepare(),
-    "Term arguments passed to step_intercept have no effect."
-  )
-
-  ex_dat2 <- data.frame(numer = rep(1, 10))
-
-  rec_trained <- recipe(~ ., data = ex_dat2) %>%
-    step_intercept() %>%
-    prepare(training = ex_dat2, verbose = FALSE)
-
-  expect_message(
-    bake(rec_trained, newdata = ex_dat2),
-    "Data appears to already contain intercept column."
+    "Selectors"
   )
 })
 
