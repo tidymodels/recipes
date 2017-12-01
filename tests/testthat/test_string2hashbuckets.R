dabd8f10db999ef448fd8f8bddafb35c9c108003
kevinykuo <kevinykuo@gmail.com>
2017-12-01 17:24:37 -0500

    add a test for string2hashbucket

--- tests/testthat/test_string2hashbuckets.R
+++ tests/testthat/test_string2hashbuckets.R
@@ -12,6 +12,17 @@ summarize_buckets <- function(num_buckets) {
 test_that("string2hashbucket basic functionality works", {
   expect_equal(nrow(summarize_buckets(1)), 1)
   expect_equal(nrow(summarize_buckets(100)), 3)
+
+  set.seed(1)
+  df <- data.frame(V1 = sample(letters, 1000, replace = TRUE),
+                   stringsAsFactors = FALSE)
+  expect_identical(recipe(~ V1, data = df) %>%
+    step_string2hashbucket(V1, num_buckets = 5) %>%
+    prep(df, stringsAsFactors = FALSE) %>%
+    bake(df) %>%
+    dplyr::count(V1) %>%
+    dplyr::pull(V1),
+    0:4L)
 })
 
 test_that("string2hashbucket throws error for bad num_buckets", {
