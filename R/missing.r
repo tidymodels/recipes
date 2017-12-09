5dfac69dd51a6db594bfde93252977bbf59b45d4
Max Kuhn <mxkuhn@gmail.com>
2017-12-09 15:57:09 -0500

    renamed to `check_missing` and made various changes

--- R/missing.r
+++ R/missing.r
@@ -1,6 +1,6 @@
 #' Check no NA Values
 #'
-#' `check_noNA` creates a a *specification* of a recipe
+#' `check_missing` creates a a *specification* of a recipe
 #'  step that will check if variables do not contain missing values.
 #'
 #' @param recipe A recipe object. The step will be added to the
@@ -11,8 +11,8 @@
 #'  currently used.
 #' @param role Not used by this step since no new variables are
 #'  created.
-#' @param trained Irrelevant for this step since for this check it does not
-#'  matter if check is trained or not.
+#' @param trained A logical for whether the selectors in `...` 
+#' have been resolved by [prep()].
 #' @return An updated version of `recipe` with the new check
 #'  added to the sequence of existing steps (if any). For the
 #'  `tidy` method, a tibble with columns `terms` (the
@@ -27,34 +27,35 @@
 #'
 #' # If the test passes, newdata is returned unaltered
 #' recipe(credit_data) %>%
-#'   check_noNA(Age, Expenses) %>%
+#'   check_missing(Age, Expenses) %>%
 #'   prep() %>%
 #'   bake(credit_data)
 #'
-#' # If your training set doesn't pass, prep() will break already
+#' # If your training set doesn't pass, prep() will stop with
+#' # an error
+#' \dontrun{
 #' recipe(credit_data)  %>%
-#'   check_noNA(Income) %>%
+#'   check_missing(Income) %>%
 #'   prep()
+#' }
 #'
-#' # If newdata contain missing values, the check will break bake()
+#' # If newdata contain missing values, the check will stop bake()
 #' train_data <- credit_data %>% dplyr::filter(Income > 150)
 #' test_data  <- credit_data %>% dplyr::filter(Income <= 150 | is.na(Income))
 #' rp <- recipe(train_data) %>%
-#'   check_noNA(Income) %>%
+#'   check_missing(Income) %>%
 #'   prep()
 #' bake(rp, train_data)
 #' bake(rp, test_data)
-check_noNA <-
+check_missing <-
   function(recipe,
            ...,
            role = NA,
            trained = FALSE,
-           means = NULL,
-           na.rm = TRUE,
            columns = NULL) {
-    add_step(
+    add_check(
       recipe,
-      check_noNA_new(
+      check_missing_new(
         terms   = check_ellipses(...),
         role    = role,
         trained = trained,
@@ -63,55 +64,52 @@ check_noNA <-
     )
   }
 
-check_noNA_new <-
+check_missing_new <-
   function(terms = NULL,
            role  = NA,
            trained = FALSE,
            columns = NULL) {
-    step(subclass = "noNA",
-         prefix   = "check_",
-         terms    = terms,
-         role     = role,
-         trained  = trained,
-         columns  = columns)
+    check(subclass = "missing",
+          prefix   = "check_",
+          terms    = terms,
+          role     = role,
+          trained  = trained,
+          columns  = columns)
   }
 
-prep.check_noNA <- function(x, training, info = NULL, ...) {
+prep.check_missing <- function(x, training, info = NULL, ...) {
   col_names <- terms_select(x$terms, info = info)
-  check_noNA_new(terms = x$terms,
-                 role  = x$role,
-                 trained = TRUE,
-                 columns = col_names)
+  check_missing_new(terms = x$terms,
+                    role  = x$role,
+                    trained = TRUE,
+                    columns = col_names)
 }
 
-bake.check_noNA <- function(object, newdata, ...) {
+bake.check_missing <- function(object, newdata, ...) {
   col_names       <- object$columns
   subset_to_check <- newdata[col_names]
   nr_na           <- colSums(is.na(subset_to_check))
   if (any(nr_na > 0)) {
     with_na     <- names(nr_na[nr_na > 0])
     with_na_str <- paste(paste0("`", with_na, "`"), collapse = ", ")
-    stop(paste0("The following columns contain missing values: ",
-                with_na_str,
-                "."),
-         call. = FALSE)
+    stop("The following columns contain missing values: ",
+         with_na_str, ".", call. = FALSE)
   }
   newdata
 }
 
-print.check_noNA <-
+print.check_missing <-
   function(x, width = max(20, options()$width - 30), ...) {
-    cat("Check no NA for ", sep = "")
-    printer(names(x$means), x$terms, x$trained, width = width)
+    cat("Check missing values for ", sep = "")
+    printer(x$columns, x$terms, x$trained, width = width)
     invisible(x)
   }
 
-#' @rdname check_noNA
-#' @param x A `check_noNA` object.
-## copied this from tidy.step_dummy, candidate for refactoring
-tidy.check_noNA <- function(x, ...) {
+#' @rdname check_missing
+#' @param x A `check_missing` object.
+tidy.check_missing <- function(x, ...) {
   if (is_trained(x)) {
-    res <- tibble(terms = names(x$levels))
+    res <- tibble(terms = x$columns)
   } else {
     res <- tibble(terms = sel2char(x$terms))
   }
