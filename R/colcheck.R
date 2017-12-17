5aaff69119b5c29e46c6adcb07e82b2dfc5eb1c8
Max Kuhn <mxkuhn@gmail.com>
2017-12-17 17:11:30 -0500

    Added a little more documentation and examples for topepo/recipes#104

--- R/colcheck.R
+++ R/colcheck.R
@@ -2,7 +2,7 @@
 #'
 #' `check_cols` creates a *specification* of a recipe
 #'  step that will check if all the columns of the training frame are
-#'  present in the newdata.
+#'  present in the new data.
 #'
 #' @param recipe A recipe object. The step will be added to the
 #'  sequence of operations for this recipe.
@@ -14,10 +14,26 @@
 #'  created.
 #' @param trained A logical to indicate if the column names to check
 #'  against are gathered.
-
-## Do we want to keep the rows in cheks_ in the first place. Will
-## never be used.
-
+#' @return An updated version of `recipe` with the new check
+#'  added to the sequence of existing operations (if any). For the
+#'  `tidy` method, a tibble with columns `terms` (the
+#'  selectors or variables selected).
+#' @export
+#' @details This check will break the `bake` function if any of the checked
+#'  columns does contain `NA` values. If the check passes, nothing is changed
+#'  to the data.
+#' @examples
+#' 
+#' data(biomass)
+#' 
+#' biomass_rec <- recipe(HHV ~ ., data = biomass) %>%
+#'    step_rm(sample, dataset) %>%
+#'    check_cols(contains("gen")) %>%
+#'    step_center(all_predictors())
+#'    
+#' \dontrun{
+#' bake(biomass_rec, biomass[, c("carbon", "HHV")])
+#' }
 check_cols <-
   function(recipe,
            ...,
@@ -74,8 +90,8 @@ print.check_cols <-
     invisible(x)
   }
 
-#' @rdname check_missing
-#' @param x A `check_missing` object.
+#' @rdname check_cols
+#' @param x A `check_cols` object.
 tidy.check_cols <- function(x, ...) {
   if (is_trained(x)) {
     res <- tibble(terms = x$columns)
