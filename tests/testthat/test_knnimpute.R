81ed30d85795021c17125349b51b12f424c3ee1f
topepo <mxkuhn@gmail.com>
2018-10-21 22:09:15 -0400

    modified args for isomap and knn imputation for #177

--- tests/testthat/test_knnimpute.R
+++ tests/testthat/test_knnimpute.R
@@ -28,13 +28,13 @@ test_that('imputation values', {
     step_knnimpute(carbon,
                    nitrogen,
                    impute_with = imp_vars(hydrogen, oxygen, nitrogen),
-                   K = 3, 
+                   neighbors = 3, 
                    id = "")
 
   imp_exp_un <- tibble(
     terms = c("carbon", "nitrogen"),
     predictors = rep(NA_character_, 2),
-    K = rep(3, 2),
+    neighbors = rep(3, 2),
     id = ""
   )
   expect_equal(imp_exp_un, tidy(impute_rec, number = 2))
@@ -51,13 +51,13 @@ test_that('imputation values', {
   impute_rec <- prep(impute_rec, training = biomass_tr, verbose = FALSE)
   imputed_te <- bake(impute_rec, newdata = biomass_te)
 
-  for(i in carb_missing) {
+  for (i in carb_missing) {
     nn_tr_ind <- nn[, i]
     nn_tr_data <- tr_data$carbon[nn_tr_ind]
     expect_equal(imputed_te$carbon[i], mean(nn_tr_data))
   }
 
-  for(i in nitro_missing) {
+  for (i in nitro_missing) {
     nn_tr_ind <- nn[, i]
     nn_tr_data <- tr_data$nitrogen[nn_tr_ind]
     expect_equal(as.character(imputed_te$nitrogen[i]),
@@ -71,7 +71,7 @@ test_that('imputation values', {
     stringsAsFactors = FALSE
   )
   imp_exp_tr <- imp_exp_tr[imp_exp_tr$terms != imp_exp_tr$predictors,]
-  imp_exp_tr$K <- 3
+  imp_exp_tr$neighbors <- 3
   imp_exp_tr$id <- ""
   imp_exp_tr <- as_tibble(imp_exp_tr)
   expect_equal(imp_exp_tr, tidy(impute_rec, number = 2))
@@ -79,9 +79,24 @@ test_that('imputation values', {
 })
 
 
+test_that('deprecated arg', {
+  expect_message(
+    discr_rec <- rec %>%
+      step_knnimpute(carbon,
+                     nitrogen,
+                     impute_with = imp_vars(hydrogen, oxygen, nitrogen),
+                     K = 3, 
+                     id = "")
+  )
+})
+
 test_that('printing', {
   discr_rec <- rec %>%
-    step_discretize(nitrogen, options = list(keep_na = FALSE))
+    step_knnimpute(carbon,
+                   nitrogen,
+                   impute_with = imp_vars(hydrogen, oxygen, nitrogen),
+                   neighbors = 3, 
+                   id = "")
   expect_output(print(discr_rec))
   expect_output(prep(discr_rec, training = biomass_tr, verbose = TRUE))
 })
