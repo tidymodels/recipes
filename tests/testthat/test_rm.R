c33a17e43e4fe62ded9302dae2bb963eaefb136a
topepo <mxkuhn@gmail.com>
2018-11-04 20:33:21 -0500

    test case for when #239 is resolved

--- tests/testthat/test_rm.R
+++ tests/testthat/test_rm.R
@@ -10,7 +10,7 @@ set.seed(12)
 ex_dat <- data.frame(x1 = rnorm(n),
                      x2 = runif(n))
 
-test_that('simple logit trans', {
+test_that('basics', {
   rec <- recipe(~., data = ex_dat) %>%
     step_rm(x1)
 
@@ -19,6 +19,21 @@ test_that('simple logit trans', {
 
   expect_equal(colnames(rec_rm), "x2")
 })
+
+# test_that('skipping', {
+#   rec <- recipe(~., data = ex_dat) %>%
+#     step_rm(x1, skip = TRUE)
+#
+#   rec_trained <- prep(rec, training = ex_dat, retain = TRUE)
+#   tr_res <- juice(rec_trained)
+#   te_res <- bake(rec_trained, newdata = ex_dat)
+#
+#   expect_equal(colnames(tr_res), "x2")
+#   expect_equal(colnames(tr_res), c("x1", "x2"))
+# })
+
+
+
 
 test_that('printing', {
   rec <- recipe(~., data = ex_dat) %>%
