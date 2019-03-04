86c4753820f487d2bd369297fe58003047ba19bf
topepo <mxkuhn@gmail.com>
2019-03-03 21:52:12 -0500

    changes to role functions
     - `update_role()` only allows `old_role = NULL` when the variables in question only contain a single role.
    
     - Using a role value of `NA` in the arguments is largely disallowed.
    
     - `remove_role()` is now used to eliminate a role or to reset the role to `NA`.

--- R/formula.R
+++ R/formula.R
@@ -1,16 +1,16 @@
 #' Create a Formula from a Prepared Recipe
-#' 
+#'
 #' In case a model formula is required, the formula method can
 #'  be used on a recipe to show what predictors and outcome(s)
-#'  could be used. 
-#'  
-#' @param x A recipe object where all steps have been prepared. 
+#'  could be used.
+#'
+#' @param x A recipe object where all steps have been prepared.
 #' @param ... Note currently used.
-#' @return A formula. 
-#' @examples 
-#' 
+#' @return A formula.
+#' @examples
+#'
 #' formula(recipe(Species + Sepal.Length ~ ., data = iris))
-#' 
+#'
 #' iris_rec <- recipe(Species ~ ., data = iris) %>%
 #'   step_center(all_numeric()) %>%
 #'   prep(training = iris)
@@ -21,13 +21,18 @@ formula.recipe <- function(x, ...) {
     stop("All steps in the recipe must be prepped before the ",
          "formula can be computed.",
          call. = FALSE)
-  
+
   x <- summary(x)
   x_vars <- x$variable[x$role == "predictor"]
+  x_vars <- x_vars[!is.na(x_vars)]
+  if (length(x_vars) == 0)
+    x_vars <- 1
   y_vars <- x$variable[x$role == "outcome"]
-  
+  y_vars <- y_vars[!is.na(y_vars)]
+  if (length(y_vars) == 0)
+    y_vars <- ""
   x_vars <- paste0(x_vars, collapse = "+")
   y_vars <- paste0(y_vars, collapse = "+")
-  
+
   as.formula(paste(y_vars, x_vars, sep = "~"))
 }
