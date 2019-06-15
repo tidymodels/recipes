9a38bae02e55702e8cb1d20050fbcc32653f4cb1
DavisVaughan <davis@rstudio.com>
2019-06-15 09:26:16 -0400

    Call `quo_get_expr()` on quosures rather than treating them as calls

--- R/misc.R
+++ R/misc.R
@@ -198,6 +198,8 @@ dummy_names <- function(var, lvl, ordinal = FALSE, sep = "_") {
 fun_calls <- function(f) {
   if (is.function(f)) {
     fun_calls(body(f))
+  } else if (is_quosure(f))  {
+    fun_calls(quo_get_expr(f))
   } else if (is.call(f)) {
     fname <- as.character(f[[1]])
     # Calls inside .Internal are special and shouldn't be included
