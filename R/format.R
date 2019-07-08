a879a6cebc2eca323dca0a73d0c58374f5e73cb0
Karl_Hailperin <khailperin@data-blitz.com>
2019-07-08 13:15:12 -0500

    Should live in their own file now that they're not internal.

---
+++ R/format.R
@@ -0,0 +1,32 @@
+## ancillary step functions
+
+## then 9 is to keep space for "[trained]"
+#' @export
+format_ch_vec <-
+  function(x,
+           sep = ", ",
+           width = options()$width - 9) {
+    widths <- nchar(x)
+    sep_wd <- nchar(sep)
+    adj_wd <- widths + sep_wd
+    if (sum(adj_wd) >= width) {
+      keepers <- max(which(cumsum(adj_wd) < width)) - 1
+      if (length(keepers) == 0 || keepers < 1) {
+        x <- paste(length(x), "items")
+      } else {
+        x <- c(x[1:keepers], "...")
+      }
+    }
+    paste0(x, collapse = sep)
+  }
+
+#' @export
+format_selectors <- function(x, width = options()$width - 9) {
+  ## convert to character without the leading ~
+  x_items <- lapply(x, function(x) {
+    as.character(get_expr(x))
+  })
+
+  x_items <- unlist(x_items)
+  format_ch_vec(x_items, width = width, sep = ", ")
+}
