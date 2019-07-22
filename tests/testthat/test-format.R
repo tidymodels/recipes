9fbe08246520b7ad6f634ed785aecda48afc20a9
Karl_Hailperin <khailperin@data-blitz.com>
2019-07-22 13:42:11 -0500

    misjudged expected output

--- tests/testthat/test-format.R
+++ tests/testthat/test-format.R
@@ -4,7 +4,7 @@ test_that("format_ch_vec handles short vectors", {
 
 test_that("format_ch_vec handles long vectors with short items", {
   expect_equal(format_ch_vec(as.character(1:1000)),
-               "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, ...")
+               "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, ...")
 })
 
 test_that("format_ch_vec handles a vector with long items", {
@@ -25,7 +25,7 @@ test_that("format_selectors handles small numbers of selectors", {
 test_that("format_ch_vec handles lots of small selectors", {
   test_exprs_small_many <- lapply(1:100, expr)
   expect_equal(format_selectors(test_exprs_small_many),
-               "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, ...")
+               "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, ...")
 })
 
 test_that("format_ch_vec handles a long expression", {
