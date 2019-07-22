c94ef38a434bc31edf1cf591792407c514cbc96b
Karl_Hailperin <khailperin@data-blitz.com>
2019-07-22 14:01:12 -0500

    redo tests to work on machines with different options$width

--- tests/testthat/test-format.R
+++ tests/testthat/test-format.R
@@ -1,10 +1,15 @@
+max_width <- width = options()$width - 9
+
 test_that("format_ch_vec handles short vectors", {
   expect_equal(format_ch_vec(as.character(1:3)), "1, 2, 3")
 })
 
 test_that("format_ch_vec handles long vectors with short items", {
-  expect_equal(format_ch_vec(as.character(1:1000)),
-               "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, ...")
+  # should fit the screen
+  expect_true(length(format_ch_vec(as.character(1:1000))) <= max_width)
+  #  should list as many items as possible, not just return "n items"
+  expect_true(format_ch_vec(as.character(1:1000)) != "1000 items")
+
 })
 
 test_that("format_ch_vec handles a vector with long items", {
@@ -24,8 +29,10 @@ test_that("format_selectors handles small numbers of selectors", {
 
 test_that("format_ch_vec handles lots of small selectors", {
   test_exprs_small_many <- lapply(1:100, expr)
-  expect_equal(format_selectors(test_exprs_small_many),
-               "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, ...")
+  # should fit the screen
+  expect_true(length(format_selectors(test_exprs_small_many)) <=  max_width)
+  #  should list as many items as possible, not just return "n items"
+  expect_true(format_selectors(test_exprs_small_many) != "1000 items")
 })
 
 test_that("format_ch_vec handles a long expression", {
