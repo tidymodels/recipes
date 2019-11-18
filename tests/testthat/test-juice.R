8a0c14bd84729d10ccb7039d382fa163d4646c7b
DavisVaughan <davis@rstudio.com>
2019-11-18 16:31:36 -0500

    Move tests to `test_basics.R`

--- tests/testthat/test-juice.R
+++
@@ -1,9 +0,0 @@
-test_that("`juice()` returns a 0 column / N row tibble when a selection returns no columns", {
-  rec <- recipe(~ ., data = iris)
-  rec <- prep(rec, iris)
-
-  expect_equal(
-    juice(rec, all_outcomes()),
-    tibble(.rows = nrow(iris))
-  )
-})
