4ff360da82116d36ff5e94bef7d5a7fba684b4ca
topepo <mxkuhn@gmail.com>
2019-11-25 09:41:51 -0500

    fixes for #415

--- R/misc.R
+++ R/misc.R
@@ -198,19 +198,24 @@ get_levels <- function(x) {
   out
 }
 
-has_lvls <- function(info)
+has_lvls <- function(info) {
   !vapply(info, function(x) all(is.na(x$values)), c(logic = TRUE))
+}
 
 strings2factors <- function(x, info) {
   check_lvls <- has_lvls(info)
-  if (!any(check_lvls))
+  if (!any(check_lvls)) {
     return(x)
+  }
   info <- info[check_lvls]
+  vars <- names(info)
+  info <- info[vars %in% names(x)]
   for (i in seq_along(info)) {
     lcol <- names(info)[i]
-    x[, lcol] <- factor(as.character(getElement(x, lcol)),
-                        levels = info[[i]]$values,
-                        ordered = info[[i]]$ordered)
+    x[, lcol] <-
+      factor(as.character(x[[lcol]]),
+             levels = info[[i]]$values,
+             ordered = info[[i]]$ordered)
   }
   x
 }
