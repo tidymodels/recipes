17a7531acca5149eec85113bbc1a6e04b7403679
Julia Silge <julia.silge@gmail.com>
2020-04-10 13:50:43 -0600

    Update check_cols() test for no more dplyr:::all.equal.tbl_df

--- tests/testthat/test_colcheck.R
+++ tests/testthat/test_colcheck.R
@@ -20,10 +20,10 @@ test_that("check_col works in the bake stage", {
   expect_error(rp1 %>% check_cols(everything()) %>% prep() %>% bake(mtcars),
                NA)
   expect_equal(rp1 %>% check_cols(everything()) %>% prep() %>% bake(mtcars),
-               mtcars)
+               tibble(mtcars[ ,c(1, 3:11, 2)]))
   expect_error(rp2 %>% check_cols(cyl, mpg, drat) %>% prep %>% bake(mtcars), NA)
   expect_equal(rp2 %>% check_cols(cyl, mpg, drat) %>% prep %>% bake(mtcars),
-               mtcars[ ,c(1, 2, 5)])
+               tibble(mtcars[ ,c(1, 5, 2)]))
   expect_error(rp1 %>% check_cols(everything()) %>% prep %>% bake(mtcars[-1]),
                "The following cols are missing from `new_data`: `mpg`.")
   expect_error(rp2 %>% check_cols(cyl, mpg, drat) %>% prep %>%
