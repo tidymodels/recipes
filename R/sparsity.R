801cc348de73fd5c416fa263ea8af03a442870d1
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2020-05-24 21:36:54 -0700

    allow for sparse lists in convert_matrix

--- R/sparsity.R
+++ R/sparsity.R
@@ -1,5 +1,10 @@
 
 convert_matrix <- function(x, sparse = TRUE) {
+  is_list <- vapply(x, is.list, logical(1))
+
+  x_lists <- x[is_list]
+  x <- x[!is_list]
+
   is_num <- vapply(x, is.numeric, logical(1))
 
   if (!all(is_num)) {
@@ -7,9 +12,9 @@ convert_matrix <- function(x, sparse = TRUE) {
     if (num_viol < 5)
       rlang::abort(
         paste0(
-        "Columns (",
-        paste0("`", names(is_num)[!is_num], "`", collapse = ", "),
-        ") are not numeric; cannot convert to matrix."
+          "Columns (",
+          paste0("`", names(is_num)[!is_num], "`", collapse = ", "),
+          ") are not numeric; cannot convert to matrix."
         )
       )
     else
@@ -17,9 +22,9 @@ convert_matrix <- function(x, sparse = TRUE) {
         paste0(
           num_viol,
           " columns are not numeric; cannot ",
-           "convert to matrix."
-          )
+          "convert to matrix."
         )
+      )
   }
 
   # Issue-206: Don't use model.matrix(~ . + 0) here as it drops NA rows unless
@@ -32,6 +37,16 @@ convert_matrix <- function(x, sparse = TRUE) {
     res <- Matrix(res, sparse = TRUE)
   }
 
+  if (length(x_lists) > 0) {
+    sparse_matrices <- purrr::map(x_lists, purrr::reduce, rbind)
+    sparse_matrices <- purrr::reduce(sparse_matrices, cbind)
+
+    res <- cbind(res, sparse_matrices)
+
+    if (!sparse) {
+      res <- as.matrix(res)
+    }
+  }
+
   res
 }
-
