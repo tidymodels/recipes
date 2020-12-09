6f0e24fa37b02ce625ff96975dc06f4193e7920b
Grégoire Denay <gregoire.denay@cvua-rrw.de>
2020-12-09 16:24:21 +0100

    Update test_pca.R
    Add "threshold" to expected tunable parameters

--- tests/testthat/test_pca.R
+++ tests/testthat/test_pca.R
@@ -147,10 +147,10 @@ test_that('tunable', {
     recipe(~ ., data = iris) %>%
     step_pca(all_predictors())
   rec_param <- tunable.step_pca(rec$steps[[1]])
-  expect_equal(rec_param$name, c("num_comp"))
+  expect_equal(rec_param$name, c("num_comp", "threshold"))
   expect_true(all(rec_param$source == "recipe"))
   expect_true(is.list(rec_param$call_info))
-  expect_equal(nrow(rec_param), 1)
+  expect_equal(nrow(rec_param), 2)
   expect_equal(
     names(rec_param),
     c('name', 'call_info', 'source', 'component', 'component_id')
