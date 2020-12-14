177ab4f1b8137ff0448606241a0be29bdfaeedce
Konrad Semsch <konrad.semsch@innogy.com>
2020-12-14 21:32:27 +0100

    worked on discussed points and added basic tests

--- R/naindicate.R
+++ R/naindicate.R
@@ -1,6 +1,6 @@
 #' Create Missing Data Column Indicators
 #'
-#' `step_na_indicate` creates a *specification* of a recipe step that will
+#' `step_indicate_na` creates a *specification* of a recipe step that will
 #'  create and append additional binary columns to the dataset, to indicate
 #'  which observations are missing.
 #'
@@ -16,7 +16,7 @@
 #' @param columns A character string of variable names that will
 #'  be populated (eventually) by the terms argument.
 #' @param prefix A character string that will be the prefix to the
-#'  resulting new variables. See notes below.
+#'  resulting new variables. Defaults to "na_ind".
 #' @return An updated version of `recipe` with the new step added to the
 #'  sequence of existing steps (if any). For the `tidy` method, a tibble with
 #'  columns `terms` (the selectors or variables selected) and `model` (the
@@ -41,27 +41,27 @@
 #' rec <- recipe(Price ~ ., data = credit_tr)
 #'
 #' impute_rec <- rec %>%
-#'   step_na_indicate(Income, Assets, Debt)
+#'   step_indicate_na(Income, Assets, Debt)
 #'
 #' imp_models <- prep(impute_rec, training = credit_tr)
 #'
 #' imputed_te <- bake(imp_models, new_data = credit_te, everything())
 
-step_na_indicate <-
+step_indicate_na <-
   function(recipe,
            ...,
-           role    = "predictor",
+           role = "predictor",
            trained = FALSE,
            columns = NULL,
-           prefix  = NULL,
-           skip    = FALSE,
-           id = rand_id("na_indicate")) {
+           prefix = "na_ind",
+           skip = FALSE,
+           id = rand_id("indicate_na")) {
 
     terms = ellipse_check(...)
 
     add_step(
       recipe,
-      step_na_indicate_new(
+      step_indicate_na_new(
         terms = terms,
         role = role,
         trained = trained,
@@ -73,67 +73,67 @@ step_na_indicate <-
     )
   }
 
-step_na_indicate_new <-
+step_indicate_na_new <-
   function(terms, role, trained, columns, prefix, skip, id) {
     step(
-      subclass = "na_indicate",
+      subclass = "indicate_na",
       terms = terms,
       role = role,
       trained = trained,
       columns = columns,
-      prefix = "na_ind",
+      prefix = prefix,
       skip = skip,
       id = id
     )
   }
 
 #' @export
-prep.step_na_indicate <- function(x, training, info = NULL, ...) {
-  col_names <- eval_select_recipes(x$terms, training, info)
+prep.step_indicate_na <- function(x, training, info = NULL, ...) {
+  col_names <- terms_select(x$terms, info)
 
   # TODO add other checks
   # check_type(training[, col_names])
 
-  step_na_indicate_new(
-    terms   = x$terms,
-    role    = x$role,
+  step_indicate_na_new(
+    terms = x$terms,
+    role = x$role,
     trained = TRUE,
     columns = col_names,
-    prefix  = x$prefix,
-    skip    = x$skip,
-    id      = x$id
+    prefix = x$prefix,
+    skip = x$skip,
+    id = x$id
   )
 }
 
 #' @export
-bake.step_na_indicate <- function(object, new_data, ...) {
+bake.step_indicate_na <- function(object, new_data, ...) {
   col_names <- object$columns
 
-  for (i in seq_along(col_names)) {
-    col <- new_data[[col_names[i]]]
-    # NOTE the original column should remain
-    new_data[, col_names[i]] <- col
-    # NOTE adding indicator columns with a prefix
-    new_data[, paste0(object$prefix, "_", col_names[i])] <- ifelse(is.na(col), 1, 0)
-  }
-  as_tibble(new_data)
+  df_ind_na <- purrr::map_dfc(
+      new_data[col_names],
+      ~ifelse(is.na(.x), 1, 0)
+    ) %>%
+    dplyr::rename_with(~paste0(object$prefix, "_", .x))
+  new_data <- dplyr::bind_cols(new_data, df_ind_na)
+
+  tibble::as_tibble(new_data)
 }
 
-print.step_na_indicate <-
+print.step_indicate_na <-
   function(x, width = max(20, options()$width - 30), ...) {
     cat("Creating missing data variable indicators for ", sep = "")
     printer(x$columns, x$terms, x$trained, width = width)
     invisible(x)
   }
 
-#' @rdname step_na_indicate
-#' @param x A `step_na_indicate` object.
+#' @rdname step_indicate_na
+#' @param x A `step_indicate_na` object.
 #' @export
 tidy.step_shadow_missing <- function(x, ...) {
   if (is_trained(x)) {
-    res <- tibble(terms = x$columns)
+    res <- tibble::tibble(terms = x$columns)
   } else {
-    res <- tibble(terms = sel2char(x$terms))
+    res <- tibble::tibble(terms = sel2char(x$terms))
   }
   res$id <- x$id
   res
