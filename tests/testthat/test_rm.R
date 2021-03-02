d2c6f26e52a5ca6b82e6366e39003fd0bcef36c8
Hannah Frick <hannah.frick@gmx.de>
2021-03-02 16:45:29 +0000

    make tests self-contained

--- tests/testthat/test_rm.R
+++ tests/testthat/test_rm.R
@@ -5,14 +5,15 @@ library(tibble)
 context("Removing variables")
 
 
-n <- 20
-set.seed(12)
-ex_dat <- data.frame(
-  x1 = rnorm(n),
-  x2 = runif(n)
-)
-
 test_that("basics", {
+
+  n <- 20
+  set.seed(12)
+  ex_dat <- data.frame(
+    x1 = rnorm(n),
+    x2 = runif(n)
+  )
+
   rec <- recipe(~., data = ex_dat) %>%
     step_rm(x1)
 
@@ -23,6 +24,14 @@ test_that("basics", {
 })
 
 # test_that('skipping', {
+#
+#   n <- 20
+#   set.seed(12)
+#   ex_dat <- data.frame(
+#     x1 = rnorm(n),
+#     x2 = runif(n)
+#   )
+#
 #   rec <- recipe(~., data = ex_dat) %>%
 #     step_rm(x1, skip = TRUE)
 #
@@ -34,32 +43,28 @@ test_that("basics", {
 #   expect_equal(colnames(tr_res), c("x1", "x2"))
 # })
 
-iris_rec <- recipe(~., data = iris)
-
-iris_train <- iris %>%
-  as_tibble() %>%
-  slice(1:75)
-iris_test <- iris %>%
-  as_tibble() %>%
-  # change the position of the variables
-  select(Species, starts_with("Sepal"), starts_with("Petal")) %>%
-  slice(76:150)
-
 
 test_that("basic usage", {
   rec <-
-    iris_rec %>%
+    recipe(~., data = iris) %>%
     step_rm(Species, starts_with("Sepal"))
 
-  prepped <- prep(rec, training = iris_train)
+  prepped <- prep(rec, training = iris %>% slice(1:75))
 
   dplyr_train <-
-    iris_train %>%
+    iris %>%
+    as_tibble() %>%
+    slice(1:75) %>%
     select(-Species, -starts_with("Sepal"))
 
   rec_train <- juice(prepped)
   expect_equal(dplyr_train, rec_train)
 
+  iris_test <- iris %>%
+    as_tibble() %>%
+    # change the position of the variables
+    select(Species, starts_with("Sepal"), starts_with("Petal")) %>%
+    slice(76:150)
   dplyr_test <-
     iris_test %>%
     select(-Species, -starts_with("Sepal"))
@@ -70,7 +75,7 @@ test_that("basic usage", {
 
 test_that("basic rename", {
   rec <-
-    iris_rec %>%
+    recipe(~., data = iris) %>%
     step_rm(sepal_length = Sepal.Length)
 
   expect_error(
@@ -81,18 +86,25 @@ test_that("basic rename", {
 
 test_that("remove via type", {
   rec <-
-    iris_rec %>%
+    recipe(~., data = iris) %>%
     step_rm(all_numeric())
 
-  prepped <- prep(rec, training = iris_train)
+  prepped <- prep(rec, training = iris %>% slice(1:75))
 
   dplyr_train <-
-    iris_train %>%
+    iris %>%
+    as_tibble() %>%
+    slice(1:75) %>%
     select_if(~ !is.numeric(.))
 
   rec_train <- juice(prepped)
   expect_equal(dplyr_train, rec_train)
 
+  iris_test <- iris %>%
+    as_tibble() %>%
+    # change the position of the variables
+    select(Species, starts_with("Sepal"), starts_with("Petal")) %>%
+    slice(76:150)
   dplyr_test <-
     iris_test %>%
     select_if(~ !is.numeric(.))
@@ -106,15 +118,22 @@ test_that("remove via role", {
     recipe(Species ~ ., data = iris) %>%
     step_rm(all_predictors())
 
-  prepped <- prep(rec, training = iris_train)
+  prepped <- prep(rec, training = iris %>% slice(1:75))
 
   dplyr_train <-
-    iris_train %>%
+    iris %>%
+    as_tibble() %>%
+    slice(1:75) %>%
     select(Species)
 
   rec_train <- juice(prepped)
   expect_equal(dplyr_train, rec_train)
 
+  iris_test <- iris %>%
+    as_tibble() %>%
+    # change the position of the variables
+    select(Species, starts_with("Sepal"), starts_with("Petal")) %>%
+    slice(76:150)
   dplyr_test <-
     iris_test %>%
     select(Species)
@@ -127,28 +146,30 @@ test_that("remove with quasi-quotation", {
   sepal_vars <- c("Sepal.Width", "Sepal.Length")
 
   rec_1 <-
-    iris_rec %>%
+    recipe(~., data = iris) %>%
     step_rm(all_of(sepal_vars))
 
-  prepped_1 <- prep(rec_1, training = iris_train)
+  prepped_1 <- prep(rec_1, training = iris %>% slice(1:75))
 
   dplyr_train <-
-    iris_train %>%
+    iris %>%
+    as_tibble() %>%
+    slice(1:75) %>%
     select(-sepal_vars)
 
   rec_1_train <- juice(prepped_1)
   expect_equal(dplyr_train, rec_1_train)
 
   rec_2 <-
-    iris_rec %>%
+    recipe(~., data = iris) %>%
     step_rm(!!sepal_vars)
 
-  prepped_2 <- prep(rec_2, training = iris_train)
+  prepped_2 <- prep(rec_2, training = iris %>% slice(1:75))
 
   rm(sepal_vars)
-  expect_error(prep(rec_1, training = iris_train))
+  expect_error(prep(rec_1, training = iris %>% slice(1:75)))
   # expect_error(
-  #   prepped_2 <- prep(rec_2, training = iris_train),
+  #   prepped_2 <- prep(rec_2, training = iris %>% slice(1:75)),
   #   regexp = NA
   # )
   rec_2_train <- juice(prepped_2)
@@ -157,7 +178,7 @@ test_that("remove with quasi-quotation", {
 
 test_that("no input", {
   expect_error(
-    iris_rec %>%
+    recipe(~., data = iris) %>%
       step_rm() %>%
       prep(training = iris),
     "Please supply at least one variable specification.See [?]selections."
@@ -165,8 +186,17 @@ test_that("no input", {
 })
 
 test_that("printing", {
+
+  n <- 20
+  set.seed(12)
+  ex_dat <- data.frame(
+    x1 = rnorm(n),
+    x2 = runif(n)
+  )
+
   rec <- recipe(~., data = ex_dat) %>%
     step_rm(x1)
   expect_output(print(rec))
   expect_output(prep(rec, training = ex_dat, verbose = TRUE))
 })
+
