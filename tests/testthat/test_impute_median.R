de3060559802b352833b83082a7feca30f633a42
Julia Silge <julia.silge@gmail.com>
2021-06-04 10:25:57 -0600

    Use vec_cast to handle new data in bake.step_impute_median

--- tests/testthat/test_impute_median.R
+++ tests/testthat/test_impute_median.R
@@ -17,7 +17,7 @@ test_that('simple median', {
   rec <- recipe(Price ~ ., data = credit_tr)
 
   impute_rec <- rec %>%
-    step_medianimpute(Age, Assets, Income, id = "")
+    step_impute_median(Age, Assets, Income, id = "")
   imputed <- prep(impute_rec, training = credit_tr, verbose = FALSE)
   te_imputed <- bake(imputed, new_data = credit_te)
 
@@ -56,14 +56,32 @@ test_that('non-numeric', {
   rec <- recipe(Price ~ ., data = credit_tr)
 
   impute_rec <- rec %>%
-    step_medianimpute(Assets, Job)
+    step_impute_median(Assets, Job)
   expect_error(prep(impute_rec, training = credit_tr, verbose = FALSE))
+})
+
+test_that('all NA values', {
+  rec <- recipe(Price ~ ., data = credit_tr)
+
+  impute_rec <- rec %>%
+    step_impute_median(Age, Assets)
+  expect_error(
+    prepped <- prep(impute_rec, training = credit_tr, verbose = FALSE),
+    NA
+  )
+  expect_error(
+    baked <- bake(prepped, new_data = credit_te %>% mutate(Age = NA)),
+    NA
+  )
+
+  expect_equal(unique(baked$Age), prepped$steps[[1]]$medians$Age)
+
 })
 
 
 test_that('printing', {
   impute_rec <- recipe(Price ~ ., data = credit_tr) %>%
-    step_medianimpute(Age, Assets, Income)
+    step_impute_median(Age, Assets, Income)
   expect_output(print(impute_rec))
   expect_output(prep(impute_rec, training = credit_tr, verbose = TRUE))
 })
