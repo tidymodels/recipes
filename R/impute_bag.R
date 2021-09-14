3eb9b6ec9fe999dbbc8c8f079d83d24385889615
Max Kuhn <mxkuhn@gmail.com>
2021-09-14 16:56:46 -0400

    re-cast the predictions due to ipred bug

--- R/impute_bag.R
+++ R/impute_bag.R
@@ -262,8 +262,8 @@ bake.step_impute_bag <- function(object, new_data, ...) {
         rlang::warn("All predictors are missing; cannot impute")
       } else {
         pred_vals <- predict(object$models[[imp_var]], pred_data)
-        pred_vals <- cast(pred_vals, new_data[[imp_var]])
-        new_data[[imp_var]] <- vec_cast(new_data[[imp_var]], pred_vals)
+        # For an ipred bug reported on 2021-09-14:
+        pred_vals <- cast(pred_vals, object$models[[imp_var]]$y)
         new_data[missing_rows, imp_var] <- pred_vals
       }
     }
