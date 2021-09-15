811b7a195b3b5754fd3553164763ce89b8e8793f
DavisVaughan <davis@rstudio.com>
2021-09-15 09:40:45 -0400

    Set testthat to 3e

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(recipes)
 
-if (requireNamespace("xml2")) {
-  test_check("recipes", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
-} else {
-  test_check("recipes")
-}
+test_check("recipes")
