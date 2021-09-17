4a8e733cca954f4f45bc38fa1ffa1c688bafbc66
DavisVaughan <davis@rstudio.com>
2021-09-17 10:39:50 -0400

    Add snapshot tests for empty `step_arrange()`

--- tests/testthat/test_arrange.R
+++ tests/testthat/test_arrange.R
@@ -73,6 +73,17 @@ test_that("empty tidying", {
   )
 })
 
+test_that("empty printing", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_arrange(rec)
+
+  expect_snapshot(rec)
+
+  rec <- prep(rec, mtcars)
+
+  expect_snapshot(rec)
+})
+
 test_that('printing', {
   rec <- iris_rec %>% step_arrange(Sepal.Length)
   expect_output(print(rec))
