5f60f07408a13497ed15138d79b8f8f9a4a476ea
DavisVaughan <davis@rstudio.com>
2021-09-17 16:17:19 -0400

    Support empty selection in `step_inverse()`

--- tests/testthat/test_inverse.R
+++ tests/testthat/test_inverse.R
@@ -41,3 +41,42 @@ test_that('printing', {
   expect_output(prep(rec, training = ex_dat, verbose = TRUE))
 })
 
+test_that("empty selection prep/bake is a no-op", {
+  rec1 <- recipe(mpg ~ ., mtcars)
+  rec2 <- step_inverse(rec1)
+
+  rec1 <- prep(rec1, mtcars)
+  rec2 <- prep(rec2, mtcars)
+
+  baked1 <- bake(rec1, mtcars)
+  baked2 <- bake(rec2, mtcars)
+
+  expect_identical(baked1, baked2)
+})
+
+test_that("empty selection tidy method works", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_inverse(rec)
+
+  expect <- tibble(
+    terms = character(),
+    id = character()
+  )
+
+  expect_identical(tidy(rec, number = 1), expect)
+
+  rec <- prep(rec, mtcars)
+
+  expect_identical(tidy(rec, number = 1), expect)
+})
+
+test_that("empty printing", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_inverse(rec)
+
+  expect_snapshot(rec)
+
+  rec <- prep(rec, mtcars)
+
+  expect_snapshot(rec)
+})
