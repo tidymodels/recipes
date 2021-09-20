b2debcc3e79e439a12f84c568259b4f444a95136
DavisVaughan <davis@rstudio.com>
2021-09-20 09:40:13 -0400

    Support empty selections for `step_nzv()`

--- tests/testthat/test_nzv.R
+++ tests/testthat/test_nzv.R
@@ -25,9 +25,7 @@ test_that('nzv filtering', {
   filtering <- rec %>%
     step_nzv(x1, x2, x3, x4, id = "")
 
-  exp_tidy_un <- tibble(terms = "")
-  exp_tidy_un$terms[1] <- NA
-  exp_tidy_un$id <- ""
+  exp_tidy_un <- tibble(terms = c("x1", "x2", "x3", "x4"), id = "")
   expect_equal(exp_tidy_un, tidy(filtering, number = 1))
 
   filtering_trained <- prep(filtering, training = dat, verbose = FALSE)
@@ -84,4 +82,41 @@ test_that('tunable', {
     names(rec_param),
     c('name', 'call_info', 'source', 'component', 'component_id')
   )
+})
+
+test_that("empty selection prep/bake is a no-op", {
+  rec1 <- recipe(mpg ~ ., mtcars)
+  rec2 <- step_nzv(rec1)
+
+  rec1 <- prep(rec1, mtcars)
+  rec2 <- prep(rec2, mtcars)
+
+  baked1 <- bake(rec1, mtcars)
+  baked2 <- bake(rec2, mtcars)
+
+  expect_identical(baked1, baked2)
+})
+
+test_that("empty selection tidy method works", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_nzv(rec)
+
+  expect <- tibble(terms = character(), id = character())
+
+  expect_identical(tidy(rec, number = 1), expect)
+
+  rec <- prep(rec, mtcars)
+
+  expect_identical(tidy(rec, number = 1), expect)
+})
+
+test_that("empty printing", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_nzv(rec)
+
+  expect_snapshot(rec)
+
+  rec <- prep(rec, mtcars)
+
+  expect_snapshot(rec)
 })
