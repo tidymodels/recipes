0e2e4dd27698d539e0e822bfdaf5a821bc2574a1
DavisVaughan <davis@rstudio.com>
2021-09-20 10:59:31 -0400

    Support empty selections for `step_scale()`

--- tests/testthat/_snaps/center_scale_norm.md
+++ tests/testthat/_snaps/center_scale_norm.md
@@ -34,6 +34,42 @@
       
       Centering for <none> [trained]
 
+# scale - empty printing
+
+    Code
+      rec
+    Output
+      Recipe
+      
+      Inputs:
+      
+            role #variables
+         outcome          1
+       predictor         10
+      
+      Operations:
+      
+      Scaling for <none>
+
+---
+
+    Code
+      rec
+    Output
+      Recipe
+      
+      Inputs:
+      
+            role #variables
+         outcome          1
+       predictor         10
+      
+      Training data contained 32 data points and no missing data.
+      
+      Operations:
+      
+      Scaling for <none> [trained]
+
 # normalize - empty printing
 
     Code
--- tests/testthat/test_center_scale_norm.R
+++ tests/testthat/test_center_scale_norm.R
@@ -58,7 +58,7 @@ test_that('scale by factor of 1 or 2', {
 
   scal_tibble_tr <-
     tibble(terms = c("carbon", "hydrogen", "oxygen", "nitrogen", "sulfur"),
-           value = sds*2,
+           value = unname(sds*2),
            id = standardized$steps[[1]]$id)
 
   expect_equal(tidy(standardized_trained, 1), scal_tibble_tr)
@@ -176,6 +176,43 @@ test_that("center - empty selection tidy method works", {
 test_that("center - empty printing", {
   rec <- recipe(mpg ~ ., mtcars)
   rec <- step_center(rec)
+
+  expect_snapshot(rec)
+
+  rec <- prep(rec, mtcars)
+
+  expect_snapshot(rec)
+})
+
+test_that("scale - empty selection prep/bake is a no-op", {
+  rec1 <- recipe(mpg ~ ., mtcars)
+  rec2 <- step_scale(rec1)
+
+  rec1 <- prep(rec1, mtcars)
+  rec2 <- prep(rec2, mtcars)
+
+  baked1 <- bake(rec1, mtcars)
+  baked2 <- bake(rec2, mtcars)
+
+  expect_identical(baked1, baked2)
+})
+
+test_that("scale - empty selection tidy method works", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_scale(rec)
+
+  expect <- tibble(terms = character(), value = double(), id = character())
+
+  expect_identical(tidy(rec, number = 1), expect)
+
+  rec <- prep(rec, mtcars)
+
+  expect_identical(tidy(rec, number = 1), expect)
+})
+
+test_that("scale - empty printing", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_scale(rec)
 
   expect_snapshot(rec)
 
