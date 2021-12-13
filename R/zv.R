6d5635a58911c694faf4c7fd48578c8035940a19
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2021-12-13 10:23:54 -0800

    clarify role of group argument

--- R/zv.R
+++ R/zv.R
@@ -7,8 +7,11 @@
 #' @param removals A character string that contains the names of
 #'  columns that should be removed. These values are not determined
 #'  until [prep.recipe()] is called.
-#' @param group An optional character string or call to [dplyr::vars()] that can be used to
-#'  specify a group(s) within which to identify variables that contain only a single value.
+#' @param group An optional character string or call to [dplyr::vars()]
+#'  that can be used to specify a group(s) within which to identify
+#'  variables that contain only a single value. If the grouping variables
+#'  are contained in terms selector, they will not be considered for
+#'  removal.
 #' @template step-return
 #' @template filter-steps
 #' @details When you [`tidy()`] this step, a tibble with column `terms` (the
