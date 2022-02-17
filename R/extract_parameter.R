bc638db203418bfb97a41ce706b1de7b2b8ba45f
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-02-17 12:51:15 -0800

    rename files to better match step names

---
+++ R/extract_parameter.R
@@ -0,0 +1,45 @@
+#' @export
+extract_parameter_set_dials.recipe <- function(x, ...) {
+  all_args <- generics::tunable(x)
+  tuning_param <- generics::tune_args(x)
+  res <-
+    dplyr::inner_join(
+      tuning_param %>% dplyr::select(-tunable),
+      all_args,
+      by = c("name", "source", "component", "component_id")
+    ) %>%
+    mutate(object = purrr::map(call_info, eval_call_info))
+
+  dials::parameters_constr(
+    res$name,
+    res$id,
+    res$source,
+    res$component,
+    res$component_id,
+    res$object
+  )
+}
+
+eval_call_info <-  function(x) {
+  if (!is.null(x)) {
+    # Look for other options
+    allowed_opts <- c("range", "trans", "values")
+    if (any(names(x) %in% allowed_opts)) {
+      opts <- x[names(x) %in% allowed_opts]
+    } else {
+      opts <- list()
+    }
+    res <- try(rlang::eval_tidy(rlang::call2(x$fun, .ns = x$pkg, !!!opts)), silent = TRUE)
+    if (inherits(res, "try-error")) {
+      stop(paste0("Error when calling ", x$fun, "(): ", as.character(res)))
+    }
+  } else {
+    res <- NA
+  }
+  res
+}
+
+#' @export
+extract_parameter_dials.recipe <- function(x, parameter, ...) {
+  extract_parameter_dials(extract_parameter_set_dials(x), parameter)
+}
