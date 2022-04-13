567165c2503d945894557eb33e37d22543f75349
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-04-12 17:25:31 -0700

    update pls test references

--- inst/pls_test_references.R
+++ inst/pls_test_references.R
@@ -21,6 +21,7 @@ cell_te <- cells %>%
   dplyr::filter(case == "Test") %>%
   dplyr::select(-case, -class)
 
+# pls 1 outcome ----------------------------------------------------------------
 bm_pls_fit <- pls(
   X = biom_tr[, -6], biom_tr[, 6], ncomp = 3, scale = TRUE
 )
@@ -33,7 +34,20 @@ bm_pls_te <- predict(bm_pls_fit, biom_te[, -6])$variates %>%
   as_tibble() %>%
   rename_with(str_replace, pattern = "dim", replacement = "PLS")
 
+# pls 2 outcomes ---------------------------------------------------------------
+bm_pls_multi_fit <- pls(
+  X = biom_tr[, -c(1, 6)], biom_tr[, c(1, 6)], ncomp = 3, scale = TRUE
+)
 
+bm_pls_multi_tr <- bm_pls_multi_fit$variates$X %>%
+  as_tibble() %>%
+  rename_with(str_replace, pattern = "comp", replacement = "PLS")
+
+bm_pls_multi_te <- predict(bm_pls_multi_fit, biom_te[, -c(1, 6)])$variates %>%
+  as_tibble() %>%
+  rename_with(str_replace, pattern = "dim", replacement = "PLS")
+
+# spls 1 outcome ---------------------------------------------------------------
 bm_spls_fit <- spls(
   X = biom_tr[, -6], biom_tr[, 6], ncomp = 3, keepX = c(3, 3, 3), scale = TRUE
 )
@@ -46,7 +60,20 @@ bm_spls_te <- predict(bm_spls_fit, biom_te[, -6])$variates %>%
   as_tibble() %>%
   rename_with(str_replace, pattern = "dim", replacement = "PLS")
 
+# spls 2 outcomes --------------------------------------------------------------
+bm_spls_multi_fit <- spls(
+  X = biom_tr[, -c(1, 6)], biom_tr[, c(1, 6)], ncomp = 3, keepX = c(3, 3, 3), scale = TRUE
+)
 
+bm_spls_multi_tr <- bm_spls_multi_fit$variates$X %>%
+  as_tibble() %>%
+  rename_with(str_replace, pattern = "comp", replacement = "PLS")
+
+bm_spls_multi_te <- predict(bm_spls_multi_fit, biom_te[, -c(1, 6)])$variates %>%
+  as_tibble() %>%
+  rename_with(str_replace, pattern = "dim", replacement = "PLS")
+
+# plsda 1 outcome --------------------------------------------------------------
 cell_plsda_fit <- plsda(
   X = cell_tr[, -1], cell_tr[[1]], ncomp = 3, scale = TRUE
 )
@@ -59,7 +86,7 @@ cell_plsda_te <- predict(cell_plsda_fit, cell_te)$variates %>%
   as_tibble() %>%
   rename_with(str_replace, pattern = "dim", replacement = "PLS")
 
-
+# splsda 1 outcome -------------------------------------------------------------
 cell_splsda_fit <- splsda(
   X = cell_tr[, -1], cell_tr[[1]], ncomp = 3, keepX = c(50, 50, 50), scale = TRUE
 )
@@ -72,11 +99,16 @@ cell_splsda_te <- predict(cell_splsda_fit, cell_te)$variates %>%
   as_tibble() %>%
   rename_with(str_replace, pattern = "dim", replacement = "PLS")
 
+# Saving -----------------------------------------------------------------------
 save(
   bm_pls_tr,
   bm_pls_te,
+  bm_pls_multi_tr,
+  bm_pls_multi_te,
   bm_spls_tr,
   bm_spls_te,
+  bm_spls_multi_tr,
+  bm_spls_multi_te,
   cell_plsda_tr,
   cell_plsda_te,
   cell_splsda_tr,
