ce1c657a10a4442662ea06d35369b9adba14eefe
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-04-27 22:29:21 -0700

    Add dedicated case weights documentation

--- man-roxygen/case-weights-unsupervised.R
+++ man-roxygen/case-weights-unsupervised.R
@@ -1,5 +1,5 @@
 #' @section Case weights:
 #'
 #' This step performs an unsupervised operation that can utilize case weights.
-#' To use them, see the documentation in case_weights and the examples on
+#' To use them, see the documentation in [case_weights] and the examples on
 #' `tidymodels.org`.
