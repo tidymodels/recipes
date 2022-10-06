0ed7232ef24e84088505f6098a19c0428a474655
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-10-06 16:36:30 -0700

    add old-get_types.R

---
+++ inst/old-get_types.R
@@ -0,0 +1,21 @@
+# remotes::install_version("recipes", "1.0.1")
+library(recipes)
+
+data("Sacramento", package = "modeldata")
+Sacramento$lgl1 <- Sacramento$beds >= 1
+Sacramento$lgl2 <- Sacramento$beds >= 2
+Sacramento$ord1 <- as.ordered(Sacramento$type)
+Sacramento$ord2 <- as.ordered(Sacramento$type)
+Sacramento$date1 <- as.Date(Sacramento$sqft, "2000-01-01")
+Sacramento$date2 <- as.Date(Sacramento$sqft, "2000-01-01")
+Sacramento$datetime1 <- as.POSIXct(Sacramento$sqft, origin = "2000-01-01 00:00:00")
+Sacramento$datetime2 <- as.POSIXct(Sacramento$sqft, origin = "2000-01-01 00:00:00")
+
+old_rec_sac <- recipe(~., data = Sacramento)
+
+# Saving -----------------------------------------------------------------------
+save(
+  old_rec_sac,
+  file = testthat::test_path("old-get_types.RData"),
+  version = 2
+)
