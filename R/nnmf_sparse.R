d0bf43b1ae8898e31af9eaefc8f812564e381651
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-11-04 12:06:01 -0700

    more all()

--- R/nnmf_sparse.R
+++ R/nnmf_sparse.R
@@ -39,7 +39,7 @@
 #'
 #' @template case-weights-not-supported
 #'
-#' @examplesIf rlang::is_installed(c("modeldata", "RcppML", "ggplot2")) && all(.Platform$OS.type!= "windows")
+#' @examplesIf all(rlang::is_installed(c("modeldata", "RcppML", "ggplot2"))) && all(.Platform$OS.type!= "windows")
 #' library(Matrix)
 #' data(biomass, package = "modeldata")
 #'
