fbd76bade14f89eecae0694da2b8d90b304f3283
simonpcouch <simonpatrickcouch@gmail.com>
2023-02-15 10:56:18 -0500

    register methods in the usual way

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-# nocov start
-
-.onLoad <- function(libname, pkgname) {
-  vctrs::s3_register("generics::tune_args", "recipe", tune_args_recipe)
-  vctrs::s3_register("generics::tune_args", "step", tune_args_step)
-  vctrs::s3_register("generics::tune_args", "check", tune_args_check)
-}
-
-# nocov end
