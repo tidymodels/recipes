f22acd8a31349b442e69b6c37cfeac3061b78b8f
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-03-06 14:06:09 -0800

    move over importFrom

--- R/0_imports.R
+++ R/0_imports.R
@@ -1,26 +1,3 @@
-#' @importFrom dplyr arrange bind_cols bind_rows count desc do filter full_join
-#' @importFrom dplyr group_by mutate mutate_at rename sample_frac sample_n
-#' @importFrom dplyr select slice summarise ungroup
-#' @importFrom gower gower_topn
-#' @importFrom ipred ipredbagg
-#' @importFrom lifecycle deprecated
-#' @importFrom lubridate decimal_date is.Date month quarter semester wday week
-#' @importFrom lubridate yday year hour minute second am
-#' @importFrom Matrix Matrix
-#' @importFrom purrr map map_chr map_dbl map_dfr map_if map_lgl reduce
-#' @importFrom rlang !! as_character call2 exec expr f_lhs f_rhs is_empty
-#' @importFrom rlang is_quosure na_dbl quo quo_get_expr quo_squash quo_text quos
-#' @importFrom rlang sym syms caller_env
-#' @importFrom splines bs ns
-#' @importFrom stats as.formula binomial complete.cases cor cov lm mahalanobis
-#' @importFrom stats median model.frame model.matrix na.omit na.pass optimize
-#' @importFrom stats poly prcomp predict quantile runif sd terms var cov.wt
-#' @importFrom tibble add_column as_tibble is_tibble tibble
-#' @importFrom tidyselect everything
-#' @importFrom utils globalVariables install.packages object.size packageVersion
-#' @importFrom utils stack
-#' @importFrom vctrs vec_cast vec_slice
-#' @importFrom withr with_seed
 
 # ------------------------------------------------------------------------------
 
