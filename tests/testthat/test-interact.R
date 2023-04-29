a9ad7da03fa35ae0c67bbcbd04f6a4ba6b6df377
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-04-28 18:53:27 -0700

    add infrastructure tests to step_interact()

--- tests/testthat/test-interact.R
+++ tests/testthat/test-interact.R
@@ -301,6 +301,43 @@ test_that("bake method errors when needed non-standard role columns are missing"
   expect_snapshot(bake(int_rec_trained, dat_tr[, 4:6]), error = TRUE)
 })
 
+test_that("empty printing", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_interact(rec)
+
+  expect_snapshot(rec)
+
+  rec <- prep(rec, mtcars)
+
+  expect_snapshot(rec)
+})
+
+test_that("empty selection prep/bake is a no-op", {
+  rec1 <- recipe(mpg ~ ., mtcars)
+  rec2 <- step_center(rec1)
+
+  rec1 <- prep(rec1, mtcars)
+  rec2 <- prep(rec2, mtcars)
+
+  baked1 <- bake(rec1, mtcars)
+  baked2 <- bake(rec2, mtcars)
+
+  expect_identical(baked1, baked2)
+})
+
+test_that("empty selection tidy method works", {
+  rec <- recipe(mpg ~ ., mtcars)
+  rec <- step_center(rec)
+
+  expect <- tibble(terms = character(), value = double(), id = character())
+
+  expect_identical(tidy(rec, number = 1), expect)
+
+  rec <- prep(rec, mtcars)
+
+  expect_identical(tidy(rec, number = 1), expect)
+})
+
 test_that("printing", {
   rec <- recipe(y ~ ., data = dat_tr) %>%
     step_interact(~ x1:x2)
