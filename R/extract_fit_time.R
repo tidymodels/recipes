4e1ca1c26de95ad81a8ca111e8fc564891c30dec
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-06-16 10:49:11 -0700

    add backwards compatibility error

--- R/extract_fit_time.R
+++ R/extract_fit_time.R
@@ -2,6 +2,12 @@
 extract_fit_time.recipe <- function(x, summarize = TRUE, ...) {
   res <- x$fit_times
 
+  if (is.null(res)) {
+    rlang::abort(
+      "This recipe was created before `extract_fit_time()` was added."
+    )
+  }
+
   if (summarize) {
     res <- tibble(
       process_id = "recipe",
