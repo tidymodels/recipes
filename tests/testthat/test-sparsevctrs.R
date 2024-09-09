82749579e568beff393e63bee4a950791f6b0ac8
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-09-09 13:19:07 -0700

    make sure that `bake()` works with sparse tibbles

--- tests/testthat/test-sparsevctrs.R
+++ tests/testthat/test-sparsevctrs.R
@@ -53,3 +53,29 @@ test_that("prep() accepts sparse tibbles", {
     is_sparse_tibble(rec$template)
   )
 })
+
+test_that("bake() accepts sparse tibbles", {
+  skip_if_not_installed("modeldata")
+
+  hotel_data <- sparse_hotel_rates()
+  hotel_data <- sparsevctrs::coerce_to_sparse_tibble(hotel_data)
+
+  rec_spec <- recipe(avg_price_per_room ~ ., data = hotel_data) %>%
+    prep()
+  
+  expect_no_condition(
+    res <- bake(rec_spec, new_data = NULL)
+  )
+
+  expect_true(
+    is_sparse_tibble(res)
+  )
+  
+  expect_no_error(
+    res <- bake(rec_spec, new_data = hotel_data)
+  )
+
+  expect_true(
+    is_sparse_tibble(res)
+  )
+})
