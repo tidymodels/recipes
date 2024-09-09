278c65ccb5a76bbaa7608e4185987872862ec89c
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-09-09 13:18:44 -0700

    make sure prep() works with sparse tibbles

--- tests/testthat/test-sparsevctrs.R
+++ tests/testthat/test-sparsevctrs.R
@@ -29,3 +29,27 @@ test_that("recipe() accepts sparse tibbles", {
   )
 })
 
+test_that("prep() accepts sparse tibbles", {
+  skip_if_not_installed("modeldata")
+
+  hotel_data <- sparse_hotel_rates()
+  hotel_data <- sparsevctrs::coerce_to_sparse_tibble(hotel_data)
+
+  rec_spec <- recipe(avg_price_per_room ~ ., data = hotel_data)
+  
+  expect_no_error(
+    rec <- prep(rec_spec)
+  )
+
+  expect_true(
+    is_sparse_tibble(rec$template)
+  )
+  
+  expect_no_error(
+    rec <- prep(rec_spec, training = hotel_data)
+  )
+
+  expect_true(
+    is_sparse_tibble(rec$template)
+  )
+})
