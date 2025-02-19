0079d95d824e4917e6ec9c4b6a87764ab8cdc309
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-02-19 13:27:18 -0800

    test step_slice() doesn't destroy sparsity

--- tests/testthat/test-slice.R
+++ tests/testthat/test-slice.R
@@ -91,6 +91,19 @@ test_that("quasiquotation", {
   expect_equal(dplyr_train, rec_2_train)
 })
 
+test_that("doesn't destroy sparsity", {
+  mtcars$vs <- sparsevctrs::as_sparse_integer(mtcars$vs)
+  mtcars$am <- sparsevctrs::as_sparse_integer(mtcars$am)
+
+  rec <- recipe(~., mtcars) %>%
+    step_slice(1:10) %>%
+    prep()
+
+  expect_false(.recipes_destroy_sparsity(rec$steps[[1]]))
+  expect_true(sparsevctrs::is_sparse_integer(bake(rec, NULL)$vs))
+  expect_true(sparsevctrs::is_sparse_integer(bake(rec, NULL)$am))
+})
+
 # Infrastructure ---------------------------------------------------------------
 
 test_that("bake method errors when needed non-standard role columns are missing", {
