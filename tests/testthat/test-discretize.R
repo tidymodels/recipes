95a2e4417e87f2306648d49d08ac2eb29eb58d88
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-03-30 15:24:45 -0700

    no longer use variant for snapshots

--- tests/testthat/test-discretize.R
+++ tests/testthat/test-discretize.R
@@ -1,8 +1,6 @@
 library(testthat)
 library(recipes)
 
-r_version <- function() paste0("R", getRversion()[, 1:2])
-
 ex_tr <- data.frame(
   x1 = 1:100,
   x2 = rep(1:5, each = 20),
@@ -141,8 +139,7 @@ test_that("multiple column prefix", {
     error = TRUE,
     recipe(~., data = example_data) |>
       step_discretize(x1, x2, options = list(labels = "hello")) |>
-      prep(),
-    variant = r_version()
+      prep()
   )
 })
 
