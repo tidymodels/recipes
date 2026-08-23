0fc4780dea9d28d884f6cbc67b45e975e0015bc7
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-08-22 17:58:04 -0700

    Allow step_regex() to select multiple columns
    bake() now loops over the selected columns, and tidy() reports the real
    per-column names instead of recycling the single result. The sparsity
    estimate no longer hardcodes a single output column.

--- R/regex.R
+++ R/regex.R
@@ -7,18 +7,18 @@
 #' @inheritParams step_pca
 #' @inheritParams step_center
 #' @inheritParams step_dummy
-#' @param ... A single selector function to choose which variable will be
-#'   searched for the regex pattern. The selector should resolve to a single
-#'   variable. See [selections()] for more details.
+#' @param ... One or more selector functions to choose which variables will be
+#'   searched for the regex pattern. See [selections()] for more details.
 #' @param pattern A character string containing a regular expression (or
 #'   character string for `fixed = TRUE`) to be matched in the given character
 #'   vector. Coerced by `as.character` to a character string if possible.
 #' @param options A list of options to [grepl()] that should not include `x` or
 #'   `pattern`.
 #' @param result A single character value for the name of the new variable. It
-#'   should be a valid column name.
-#' @param input A single character value for the name of the variable being
-#'   searched. This is `NULL` until computed by [prep()].
+#'   should be a valid column name. If the selection resolves to more than one
+#'   variable, the new variables are named `{variable}_{result}` instead.
+#' @param input The names of the variables being searched. This is `NULL` until
+#'   computed by [prep()].
 #' @template step-return
 #' @details
 #'
@@ -53,6 +53,15 @@
 #' with_dummies
 #' tidy(rec, number = 1)
 #' tidy(rec2, number = 1)
+#'
+#' # When more than one variable is selected, the new variables are named
+#' # `{variable}_{result}`
+#' covers$description2 <- rev(covers$description)
+#'
+#' recipe(~., covers) |>
+#'   step_regex(description, description2, pattern = "rock", result = "rocks") |>
+#'   prep() |>
+#'   bake(new_data = NULL)
 step_regex <- function(
   recipe,
   ...,
@@ -78,21 +87,10 @@ step_regex <- function(
     )
   }
 
-  terms <- enquos(...)
-  if (length(terms) > 1) {
-    cli::cli_abort(
-      c(
-        x = "For this step, only a single selector can be used.",
-        i = "The following {length(terms)} selectors were used: \\
-          {.var {as.character(terms)}}."
-      )
-    )
-  }
-
   add_step(
     recipe,
     step_regex_new(
-      terms = terms,
+      terms = enquos(...),
       role = role,
       trained = trained,
       pattern = pattern,
@@ -139,8 +137,8 @@ step_regex_new <-
 
 #' @export
 prep.step_regex <- function(x, training, info = NULL, ...) {
-  col_name <- recipes_eval_select(x$terms, training, info)
-  check_type(training[, col_name], types = c("string", "factor", "ordered"))
+  col_names <- recipes_eval_select(x$terms, training, info)
+  check_type(training[, col_names], types = c("string", "factor", "ordered"))
   check_string(x$pattern, arg = "pattern", allow_empty = FALSE)
   check_sparse_arg(x$sparse)
   check_options(x$options, exclude = c("x", "pattern"))
@@ -151,7 +149,7 @@ prep.step_regex <- function(x, training, info = NULL, ...) {
     trained = TRUE,
     pattern = x$pattern,
     options = x$options,
-    input = col_name,
+    input = col_names,
     result = x$result,
     sparse = x$sparse,
     keep_original_cols = get_keep_original_cols(x),
@@ -162,39 +160,47 @@ prep.step_regex <- function(x, training, info = NULL, ...) {
 
 #' @export
 bake.step_regex <- function(object, new_data, ...) {
-  col_name <- names(object$input)
-  if (length(col_name) == 0) {
+  col_names <- names(object$input)
+  if (length(col_names) == 0) {
     return(new_data)
   }
 
-  check_new_data(col_name, object, new_data)
+  check_new_data(col_names, object, new_data)
 
-  ## sub in options
-  regex <- expr(
-    grepl(
-      x = new_data[[col_name]],
-      pattern = object$pattern,
-      ignore.case = FALSE,
-      perl = FALSE,
-      fixed = FALSE,
-      useBytes = FALSE
+  new_names <- multi_result_names(col_names, object$result)
+
+  cols <- list()
+
+  for (i in seq_along(col_names)) {
+    ## sub in options
+    regex <- expr(
+      grepl(
+        x = new_data[[col_names[i]]],
+        pattern = object$pattern,
+        ignore.case = FALSE,
+        perl = FALSE,
+        fixed = FALSE,
+        useBytes = FALSE
+      )
     )
-  )
-  if (length(object$options) > 0) {
-    regex <- rlang::call_modify(regex, !!!object$options)
+    if (length(object$options) > 0) {
+      regex <- rlang::call_modify(regex, !!!object$options)
+    }
+
+    res <- ifelse(eval(regex), 1L, 0L)
+
+    if (sparse_is_yes(object$sparse)) {
+      res <- sparsevctrs::as_sparse_integer(res)
+    }
+
+    cols[[new_names[i]]] <- res
   }
 
-  new_values <- tibble::tibble(!!object$result := ifelse(eval(regex), 1L, 0L))
+  cols <- tibble::new_tibble(cols, nrow = nrow(new_data))
 
-  if (sparse_is_yes(object$sparse)) {
-    new_values[[object$result]] <- sparsevctrs::as_sparse_integer(
-      new_values[[object$result]]
-    )
-  }
-
-  new_values <- check_name(new_values, new_data, object, object$result)
-  new_data <- vec_cbind(new_data, new_values, .name_repair = "minimal")
-  new_data <- remove_original_cols(new_data, object, col_name)
+  cols <- check_name(cols, new_data, object, new_names)
+  new_data <- vec_cbind(new_data, cols, .name_repair = "minimal")
+  new_data <- remove_original_cols(new_data, object, col_names)
   new_data
 }
 
@@ -211,17 +217,17 @@ print.step_regex <-
 #' @rdname tidy.recipe
 #' @export
 tidy.step_regex <- function(x, ...) {
-  term_names <- sel2char(x$terms)
-  p <- length(term_names)
   if (is_trained(x)) {
+    col_names <- names(x$input)
     res <- tibble(
-      terms = term_names,
-      result = rep(unname(x$result), p)
+      terms = col_names,
+      result = multi_result_names(col_names, unname(x$result))
     )
   } else {
+    term_names <- sel2char(x$terms)
     res <- tibble(
       terms = term_names,
-      result = rep(na_chr, p)
+      result = rep(na_chr, length(term_names))
     )
   }
   res$id <- x$id
@@ -230,10 +236,8 @@ tidy.step_regex <- function(x, ...) {
 
 #' @export
 .recipes_estimate_sparsity.step_regex <- function(x, data, ...) {
-  lapply(1, function(n_lvl) {
-    c(
-      n_cols = n_lvl,
-      sparsity = 0.5
-    )
-  })
+  lapply(
+    seq_len(ncol(data)),
+    function(x) c(n_cols = 1, sparsity = 0.5)
+  )
 }
